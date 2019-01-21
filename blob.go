a846f9291b8d3a20ef27c37af3caedd1a7c0d2fa
Atlas Kerr <atlaskerr@gmail.com>
2019-01-20 23:26:30 -0600

    Change PutBlob parameters.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- blob.go
+++ blob.go
@@ -10,5 +10,5 @@ type Blob interface {
 	VerifyBlob(repo string, digest digest.Digest) (bool, error)
 	GetBlob(repo string, digest digest.Digest) (io.Reader, error)
 	DeleteBlob(repo string, digest digest.Digest) error
-	PutBlob(repo string, digest digest.Digest, blob io.Reader) error
+	PutBlob(repo string, digest digest.Digest, blob io.Writer) error
 }
