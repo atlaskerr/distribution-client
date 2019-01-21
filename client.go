4e70e2a8d7baabe9070b9db2ba3c9ccb3f376f82
Atlas Kerr <atlaskerr@gmail.com>
2019-01-21 15:28:56 -0600

    Add authentication methods.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- client.go
+++ client.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -18,26 +19,57 @@ var DefaultTransport = http.Transport{ResponseHeaderTimeout: time.Second}
 
 // Client is an implementation of http.RoundTripper.
 type Client struct {
-	Endpoint  *url.URL
-	Transport http.RoundTripper
+	BaseEndpoint *url.URL
+	Transport    http.RoundTripper
+	Auth         Authenticator
 }
 
 // Config defines the parameters for Client configuration.
 type Config struct {
-	Endpoint  string
-	Transport http.RoundTripper
+	BaseEndpoint string
+	Transport    http.RoundTripper
+	Auth         Authenticator
+}
+
+// Authenticator is the interface all auth methods must satisfy.
+type Authenticator interface {
+	Set(*http.Request)
+}
+
+// BasicAuth is an implementation of the Authenticator interface for
+// username/password authentication.
+type BasicAuth struct {
+	Username string
+	Password string
+}
+
+// Set adds an authentication header to a request.
+func (a *BasicAuth) Set(req *http.Request) {
+	req.SetBasicAuth(a.Username, a.Password)
+}
+
+// TokenAuth is an implementation of the Authenticator interface for token
+// authentication.
+type TokenAuth struct {
+	Token string
+}
+
+// Set add an authentication header to a request.
+func (a *TokenAuth) Set(req *http.Request) {
+	bearer := strings.Join([]string{"bearer", a.Token}, " ")
+	req.Header.Set("Authorization", bearer)
 }
 
 // New takes a Config and returns a fully initialized Client.
 func New(cfg Config) (*Client, error) {
-	host, err := url.Parse(cfg.Endpoint)
+	host, err := url.Parse(cfg.BaseEndpoint)
 	if err != nil {
 		return nil, ErrURLInvalid
 	}
 
 	c := &Client{
-		Endpoint:  host,
-		Transport: cfg.Transport,
+		BaseEndpoint: host,
+		Transport:    cfg.Transport,
 	}
 	return c, nil
 }
@@ -47,4 +79,3 @@ func New(cfg Config) (*Client, error) {
 type Distribution interface {
 	Verify() (bool, error)
 }
-
