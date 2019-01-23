495d242ff232da5596600732b736d09252c442af
Atlas Kerr <atlaskerr@gmail.com>
2019-01-23 10:19:59 -0600

    Refactor verify test-case for 100% coverage.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- verify_test.go
+++ verify_test.go
@@ -6,40 +6,53 @@ import (
 	"testing"
 )
 
-//func TestOCIVerify(t *testing.T) {
-//	tt := []struct {
-//		name      string
-//		transport http.RoundTripper
-//		handler   http.Handler
-//	}{
-//		{""},
-//	}
-//}
-
-func TestOCIVerify1(t *testing.T) {
-	srv := httptest.NewServer(http.HandlerFunc(verifyHandler))
-	defer srv.Close()
-
-	cfg := Config{
-		Host: srv.URL,
-		Auth: &TokenAuth{
-			Token: "token",
-		},
+func TestOCIVerify(t *testing.T) {
+	tt := []struct {
+		name      string
+		transport http.RoundTripper
+		handler   http.HandlerFunc
+		valid     bool
+	}{
+		{"golden", &DefaultTransport, testVerifyHandler, true},
+		{"bad round trip", &badTransport{}, testVerifyHandler, false},
+		{"verify failed", &DefaultTransport, testInvalidVerifyHandler, false},
 	}
-	c, err := New(cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	api := NewDistributionAPI(c)
-	err = api.Verify()
-	if err != nil {
-		t.Fatal(err)
+
+	for _, tc := range tt {
+		tf := func(t *testing.T) {
+			srv := httptest.NewServer(http.HandlerFunc(tc.handler))
+			defer srv.Close()
+
+			cfg := Config{
+				Host:      srv.URL,
+				Transport: tc.transport,
+				Auth:      &TokenAuth{"token"},
+			}
+			c, err := New(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			api := NewDistributionAPI(c)
+			err = api.Verify()
+			if err == nil && !tc.valid {
+				t.Fatal("expected invalid test-case")
+			}
+		}
+		t.Run(tc.name, tf)
 	}
 }
 
-func verifyHandler(w http.ResponseWriter, req *http.Request) {
+func testVerifyHandler(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path == "/v2" {
 		w.Header().Set(HeaderVersionCheck, "registry/2.0")
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func testInvalidVerifyHandler(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path == "/v2" {
+		w.Header().Set(HeaderVersionCheck, "invalid")
 		w.WriteHeader(http.StatusOK)
 	}
 }
