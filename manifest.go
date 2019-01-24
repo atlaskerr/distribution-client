d5b4f3c90056df707754857fec0400d5ed36a3af
Atlas Kerr <atlaskerr@gmail.com>
2019-01-24 12:00:29 -0600

    Add GetManifest method.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- manifest.go
+++ manifest.go
@@ -1,15 +1,16 @@
 package client
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"path"
-	//"strings"
+	"strings"
 
 	ispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 type Manifest interface {
-	GetManifest(repo, reference string) (ispec.Manifest, error)
 	DeleteManifest(repo, reference string) error
 	PutManifest(repo, reference string, manifest ispec.Manifest) error
 }
@@ -38,30 +39,37 @@ func (api *DistributionAPI) VerifyManifest(repo, reference string) error {
 	return nil
 }
 
-//func (api *DistributionAPI) GetManifest(
-//	repo string, reference string) (ispec.Manifest, error) {
-//	c := api.client
-//
-//	u := *c.Host
-//	u.Path = path.Join("/v2", repo, "manifests", reference)
-//
-//	req := &http.Request{
-//		Method: "GET",
-//		URL:    &u,
-//		Header: make(http.Header),
-//	}
-//
-//	manifestSlice := []string{
-//		ispec.MediaTypeImageManifest,
-//		ispec.MediaTypeImageIndex,
-//	}
-//
-//	acceptHeaderValue := strings.Join(manifestSlice, ",")
-//	req.Header.Set(headerAccept, acceptHeaderValue)
-//
-//	resp, err := c.RoundTrip(req)
-//	if err != nil {
-//		return err
-//	}
-//
-//}
+// GetManifest retrieves a manifest from a remote registry.
+func (api *DistributionAPI) GetManifest(
+	repo string, reference string) (*ispec.Manifest, error) {
+	c := api.client
+
+	u := *c.Host
+	u.Path = path.Join("/v2", repo, "manifests", reference)
+
+	req := &http.Request{
+		Method: "GET",
+		URL:    &u,
+		Header: make(http.Header),
+	}
+
+	manifestSlice := []string{
+		ispec.MediaTypeImageManifest,
+		ispec.MediaTypeImageIndex,
+	}
+
+	acceptHeaderValue := strings.Join(manifestSlice, ",")
+	req.Header.Set(headerAccept, acceptHeaderValue)
+
+	resp, err := c.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	var m *ispec.Manifest
+	if err := json.Unmarshal(b, m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
