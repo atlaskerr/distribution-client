d377b6e023852d953cd0f77a952b6ef5b75d42d4
Atlas Kerr <atlaskerr@gmail.com>
2019-01-23 22:03:28 -0600

    Add blob error.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- errors.go
+++ errors.go
@@ -8,7 +8,11 @@ var (
 	// compliance verification check.
 	ErrNotOCI = errors.New("distribution: registry isn't OCI-compliant")
 
-	//ErrManifestNotExist is an error returned when a manifest does not exist in
-	//a repository.
+	// ErrManifestNotExist is an error returned when a manifest does not exist in
+	// a repository.
 	ErrManifestNotExist = errors.New("distribution: manifest does not exist")
+
+	// ErrBlobNotExist is an error returned when a blob does not exist in
+	// a repository.
+	ErrBlobNotExist = errors.New("distribution: blob does not exist")
 )
