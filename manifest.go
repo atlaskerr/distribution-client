07aee2af8c9a468d35253416900ad4c10d52dadf
Atlas Kerr <atlaskerr@gmail.com>
2019-01-30 22:44:58 -0600

    Add getManifest method.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- manifest.go
+++ manifest.go
@@ -8,14 +8,10 @@ import (
 	"path"
 	"strings"
 
+	digest "github.com/opencontainers/go-digest"
 	ispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/xeipuuv/gojsonschema"
 )
-
-type Manifest interface {
-	DeleteManifest(repo, reference string) error
-	PutManifest(repo, reference string, manifest ispec.Manifest) error
-}
 
 // VerifyManifest confirms the existance of a manifest in a remote registry.
 func (api *DistributionAPI) VerifyManifest(repo, reference string) error {
@@ -75,18 +71,55 @@ func (api *DistributionAPI) GetManifests(
 	mediaType := resp.Header.Get("Content-Type")
 
 	if mediaType == ispec.MediaTypeImageIndex {
-		_, err = api.parseIndex(resp.Body)
+		idx, err := api.parseIndex(resp.Body)
 		if err != nil {
 			return nil, nil, err
 		}
+
+		for _, desc := range idx.Manifests {
+			_, _ = api.getManifest(repo, desc.Digest)
+		}
+
 	}
 
 	return nil, nil, ErrUnknownMediaType
 }
 
-func (api *DistributionAPI) getManifests(idx ispec.Index) (*[]ispec.Manifest, error) {
-	var m []ispec.Manifest
-	return &m, nil
+func (api *DistributionAPI) getManifest(repo string, digest digest.Digest) (*ispec.Manifest, error) {
+	c := api.client
+
+	u := *c.Host
+	u.Path = path.Join("/v2", repo, "manifests", digest.String())
+
+	req := &http.Request{
+		Method: "GET",
+		URL:    &u,
+		Header: make(http.Header),
+	}
+	resp, err := c.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, ErrParseBody
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		var errorResp *ErrorResponse
+		if err := json.Unmarshal(b, errorResp); err != nil {
+			return nil, ErrParseJSON
+		}
+		return nil, errorResp
+	}
+
+	var manifest *ispec.Manifest
+	if err := json.Unmarshal(b, manifest); err != nil {
+		return nil, ErrParseJSON
+	}
+	return manifest, nil
 }
 
 func (api *DistributionAPI) parseIndex(idx io.Reader) (*ispec.Index, error) {
