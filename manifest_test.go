48bd91f433200cf209e6cd53631fc453080ce85d
Atlas Kerr <atlaskerr@gmail.com>
2019-01-31 17:35:22 -0600

    Add GetManifest test-case scaffolding.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- manifest_test.go
+++ manifest_test.go
@@ -54,3 +54,37 @@ func testVerifyManifestNotFoundHandler(w http.ResponseWriter, req *http.Request)
 		w.WriteHeader(http.StatusNotFound)
 	}
 }
+
+func TestGetManifests(t *testing.T) {
+	tt := []struct {
+		name      string
+		transport http.RoundTripper
+		handler   http.HandlerFunc
+		valid     bool
+	}{}
+
+	for _, tc := range tt {
+		tf := func(t *testing.T) {
+			h := http.HandlerFunc(tc.handler)
+			srv := httptest.NewServer(h)
+			defer srv.Close()
+
+			cfg := Config{
+				Host:      srv.URL,
+				Transport: tc.transport,
+				Auth:      &TokenAuth{"token"},
+			}
+			c, err := New(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			api := NewDistributionAPI(c)
+			_, _, err = api.GetManifests("foo/bar", "latest")
+			if err == nil && !tc.valid {
+				t.Fatal("expected invalid test-case")
+			}
+		}
+		t.Run(tc.name, tf)
+	}
+}
