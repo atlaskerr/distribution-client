896b114c223e94aa7e78fe68bdb486c0eede80ad
Atlas Kerr <atlaskerr@gmail.com>
2019-01-30 22:43:52 -0600

    Add custom error for registry error responses.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- errors.go
+++ errors.go
@@ -1,28 +1,35 @@
 package client
 
-import "errors"
+import (
+	"errors"
+)
+
+// Error satisfies the Error interface.
+func (er *ErrorResponse) Error() string {
+	return "distribution: registry returned error"
+}
+
+func (er *ErrorResponse) Detail() []ErrorInfo {
+	return er.Errors
+}
+
+type ErrorResponse struct {
+	Errors []ErrorInfo `json:"errors"`
+}
+
+type ErrorInfo struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Detail  string `json:"detail,omitempty"`
+}
 
 var (
-
-	// ErrNotOCI is an error returned when a remote registry fails an OCI
-	// compliance verification check.
-	ErrNotOCI = errors.New("distribution: registry isn't OCI-compliant")
-
-	// ErrManifestNotExist is an error returned when a manifest does not exist in
-	// a repository.
+	ErrNotOCI           = errors.New("distribution: registry isn't OCI-compliant")
 	ErrManifestNotExist = errors.New("distribution: manifest does not exist")
-
-	// ErrBlobNotExist is an error returned when a blob does not exist in
-	// a repository.
-	ErrBlobNotExist = errors.New("distribution: blob does not exist")
-
-	ErrParseBody = errors.New("distribtuion: could not read response body")
-
-	ErrParseJSON = errors.New("distribution: could not parse JSON")
-
+	ErrBlobNotExist     = errors.New("distribution: blob does not exist")
+	ErrParseBody        = errors.New("distribtuion: could not read response body")
+	ErrParseJSON        = errors.New("distribution: could not parse JSON")
 	ErrUnknownMediaType = errors.New("distribution: unknown media type")
-
 	ErrSchemaValidation = errors.New("distribution: unable to validate schema")
-
-	ErrInvalidIndex = errors.New("distribution: invalid image index")
+	ErrInvalidIndex     = errors.New("distribution: invalid image index")
 )
