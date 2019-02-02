8122656015bed25d8f1ad321d22889ea5926348d
Atlas Kerr <atlaskerr@gmail.com>
2019-02-02 17:20:56 -0600

    Add helper function for accept header.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- headers.go
+++ headers.go
@@ -1,9 +1,21 @@
 package client
 
+import (
+	"strings"
+)
+
 var (
 	// HeaderVersionCheck is the header defined in the distribution spec that
 	// clients use to verify that a registry is OCI-compliant.
 	headerVersionCheck = "Docker-Distribution-Api-Version"
-
-	headerAccept = "Accept"
 )
+
+func allowMediaTypes(req *http.Request, mediaTypes ...string) {
+	allow := make([]string, len(mediaTypes))
+	for _, mt := range mediaTypes {
+		allow = append(allow, mt)
+	}
+
+	accept := strings.Join(allow, ",")
+	req.Header.Set("Accept", accept)
+}
