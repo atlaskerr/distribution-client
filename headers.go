6143719b90bccd0bbf0279ee3076ada7e8b2f931
Atlas Kerr <atlaskerr@gmail.com>
2019-02-02 19:51:29 -0600

    Remove allowMediaTypes function.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

--- headers.go
+++ headers.go
@@ -1,21 +1,7 @@
 package client
 
-import (
-	"strings"
-)
-
 var (
 	// HeaderVersionCheck is the header defined in the distribution spec that
 	// clients use to verify that a registry is OCI-compliant.
 	headerVersionCheck = "Docker-Distribution-Api-Version"
 )
-
-func allowMediaTypes(req *http.Request, mediaTypes ...string) {
-	allow := make([]string, len(mediaTypes))
-	for _, mt := range mediaTypes {
-		allow = append(allow, mt)
-	}
-
-	accept := strings.Join(allow, ",")
-	req.Header.Set("Accept", accept)
-}
