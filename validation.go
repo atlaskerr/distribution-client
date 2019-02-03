0003fdeca86f710ee3e75162d8524794252f3aea
Atlas Kerr <atlaskerr@gmail.com>
2019-02-02 19:51:15 -0600

    Add schema validation.
    Signed-off-by: Atlas Kerr <atlaskerr@gmail.com>

---
+++ validation.go
@@ -0,0 +1,40 @@
+package client
+
+import (
+	"io"
+
+	ischema "github.com/atlaskerr/oci-schemas"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+var (
+	imageIndexSchema    = ischema.ImageIndexSchema()
+	imageManifestSchema = ischema.ImageManifestSchema()
+)
+
+func validateIndex(data io.Reader) error {
+	return validate(data, imageIndexSchema)
+}
+
+func validateManifest(data io.Reader) error {
+	return validate(data, imageManifestSchema)
+}
+
+func validate(data io.Reader, schema gojsonschema.Schema) error {
+	b, err := ioutil.ReadAll(data)
+	if err != nil {
+		return err
+	}
+
+	loader := gojsonschema.NewBytesLoader(b)
+	res, err := schema.Validate(loader)
+	if err != nil {
+		return ErrSchemaValidation
+	}
+
+	if !res.Valid() {
+		return ValidationError(res.Errors())
+	}
+
+	return nil
+}
